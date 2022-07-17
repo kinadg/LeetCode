865d667dd4a154458df8d5bfe4c33490eebd2cbf
kinadg <108034210+kinadg@users.noreply.github.com>
2022-07-16 23:59:31 -0700

    Finish 1.TwoSum

--- 1.TwoSum.js
+++ 1.TwoSum.js
@@ -28,15 +28,32 @@ Output: [0,1]
 // ------------------------------ SOLUTION ------------------------------
 
 // Example 1:
-nums = [2, 7, 11, 15];
-target = 9;
+// nums = [2, 7, 11, 15];
+// target = 9;
 
 // Example 2:
-// nums = [3,2,4]
-// target = 6
+nums = [3, 2, 4];
+target = 6;
 
 // Example 3:
 // nums = [3,3]
 // target = 6
 
-var twoSum = function (nums, target) {};
+var twoSum = function (nums, target) {
+  // 1st for loop to iterate "nums" parameter (for the current value)
+  for (let i = 0; i < nums.length; i++) {
+    // 2nd for loop to iterate "nums" parameter (for the next value)
+    for (let j = i + 1; j < nums.length; j++) {
+      // compare if sum of the current value + the next value equals to the target
+      if (nums[i] + nums[j] === target) {
+        let tempArr = [];
+        // push the indexes of the values into the empty array
+        tempArr.push(i, j);
+        return tempArr;
+        // return [i, j];
+      }
+    }
+  }
+};
+
+console.log(twoSum(nums, target));
